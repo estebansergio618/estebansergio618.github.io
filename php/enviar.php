9ef67b960c17f10ed7d9c2d418fa4f4de6472e0e
Sergio Esteban <67118105+estebansergio618@users.noreply.github.com>
2023-01-14 14:03:37 -0300

    Ene 14 2023 14:03
    Hice funcionar el fomrulario

---
+++ php/enviar.php
@@ -0,0 +1,11 @@
+<?php 
+	$nombre = $_POST['nombre'];
+	$email = $_POST['email'];
+	$asunto = "WEB PERSONAL SERGIO, ".$_POST['asunto'];
+    $telefono = $_POST['telefono'];
+	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['mensaje'];
+
+	if(mail('semunoz@dc.uba.ar', $asunto, $mensaje)){
+		echo "Email sent";
+	}
+ ?>
